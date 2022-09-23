9d9503540d200dc3ce1b9701ae0527bedf87f3d2
Akshit1311 <a4748g@gmail.com>
2022-09-23 18:54:14 +0530

    serve: frontend from express app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const publicPath = path.join(__dirname, "public");
+
+app.get("*", function (req, res) {
+  res.sendFile("index.html", { root: publicPath });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 5000");
+});
